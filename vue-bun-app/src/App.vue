d6b93aa55a56d608bcbdd0228aeb6a1911e7e87d
SnowvyTH <supanat617@gmail.com>
2026-01-26 22:54:20 +0700

    Day 1.2

--- vue-bun-app/src/App.vue
+++ vue-bun-app/src/App.vue
@@ -8,10 +8,14 @@
   <ul>
     <li v-for="item in items" :key="item">{{ item }}</li>
   </ul>
+
+  <h1>Double: {{ double }}</h1>
 </template>
 
 <script setup lang="ts">
-  import { reactive, ref } from 'vue'
+  import { reactive, ref, computed, watch, watchEffect, onMounted, onUpdated } from 'vue'
+
+
   const count = ref(0)
   const myuser = ref({
     name: 'Jane Doe',
@@ -26,6 +30,32 @@
 
   // เก็บ array
   const items = ref([1, 2, 3, 4, 5])
+
+  // ตัวอย่างการใช้ computed
+  const double = computed(() => count.value * 2)
+
+  // ตัวอย่างการใช้ watch
+  watch(() => count.value, (newVal, oldVal) => {
+    console.log(`Count changed from ${oldVal} to ${newVal}`)
+  })
+
+  //ตัวอย่าง watchEffect
+  watchEffect(() => {
+    console.log(`The current count is: ${count.value}`)
+  })
+
+  // ตัวอย่างการใช้ onMounted
+  onMounted(() => {
+    console.log('Component has been mounted.')
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  })
+
+  //ตัวอย่างการใช้ onUpdate
+  onUpdated(() => {
+    console.log('Component has been updated.')
+  })
 
 </script>
 
---
+++ vue-bun-app/src/components/ChildComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Child Component</h2>
+</template>
+
+<script setup lang="ts">
+    import { onUnmounted } from 'vue';
+    let timer = setInterval(() => {
+        console.log('Interval Running');
+    }, 1000);
+
+    onUnmounted(() => {
+        clearInterval(timer);
+        console.log('Interval cleared on component unmount');
+    });
+</script>
+
+<style scoped>
+
+</style>
